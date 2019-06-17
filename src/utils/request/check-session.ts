7373530e499715da171de793dca4060d840cf735
shine <xiaoshaoqq@gmail.com>
2019-06-18 01:56:58 +0800

    tslint

--- src/utils/request/check-session.ts
+++ src/utils/request/check-session.ts
@@ -1,4 +1,4 @@
-import { doLogin } from '../../api/login'
-import { SESSION_KEY } from '../../config/global-config'
+// import { doLogin } from '../../api/login'
+// import { SESSION_KEY } from '../../config/global-config'
 
 export function checkSession(): any {}
