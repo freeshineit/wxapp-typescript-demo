748524a1fcb145298a010dd519fe2edddbba0d6d
shine <xiaoshaoqq@gmail.com>
2019-07-31 17:58:57 +0800

    更新request 请求

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,58 +1,71 @@
-import { BASEHONST, APIKEY } from '../config/index'
+import { api_url } from '../config/index'
 
-const methods = ['get', 'post', 'put', 'delete']
+export interface RequestOption {
+  url: string
+  data?: string | object | ArrayBuffer
+  dataType?: 'json' | '其他'
+  header?: object
+  method?:
+    | 'OPTIONS'
+    | 'GET'
+    | 'HEAD'
+    | 'POST'
+    | 'PUT'
+    | 'DELETE'
+    | 'TRACE'
+    | 'CONNECT'
+  responseType?: 'text' | 'arraybuffer'
+}
 
-let Request: any = {}
+class Request {
+  private baseRestUrl: string
+  constructor() {
+    this.baseRestUrl = api_url
+  }
 
-methods.forEach(item => {
-  Request[item] = (
-    url: string,
-    data?: string | object | ArrayBuffer,
-    options?: wx.RequestOption
-  ) => {
-    /* eslint-disable */
+  //http 请求类, 当noRefech为true时，不做未授权重试机制
+  request(params: RequestOption) {
+    let url = this.baseRestUrl + params.url
+    let header = {
+      'content-type': 'application/json'
+      // toekn
+    }
+    let method = params.method || 'GET'
+    let data = params.data
+
+    if (params.header) {
+      header = Object.assign(header, params.header)
+      delete params['header']
+    }
+
     return new Promise((resolve, reject) => {
-      const _url = /^http[s]?:\/\/|^\/\//.test(url) ? url : `${BASEHONST}${url}`
-
-      let _options = {}
-
-      if (options) {
-        _options = Object.assign(_options, options)
-      }
-
-      data = Object.assign(
-        {
-          apikey: APIKEY
+      wx.request({
+        ...params,
+        url: url,
+        data,
+        method,
+        header,
+        success: function(res) {
+          //   if (res.statusCode == 401) {
+          //     // 未授权
+          //   }
+          resolve(res)
+          //   // 判断以2（2xx)开头的状态码为正确
+          //   // 异常不要返回到回调中，就在request中处理，记录日志并showToast一个统一的错误即可
+          //   var code = res.statusCode.toString()
+          //   var startChar = code.charAt(0)
+          //   if (startChar == '2') {
+          //     params.success && params.success(res.data)
+          //   } else {
+          //     params.error && params.error(res)
+          //   }
         },
-        data
-      )
-
-      const requestOptions = Object.assign(
-        {
-          url: _url,
-          method: item || 'GET',
-          data,
-          success(res: wx.RequestSuccessCallbackResult) {
-            // if (res.data.code == 401) {
-            //   let currentPage = pages[pages.length - 1] // 获取当前页面的对象
-            //   if (currentPage.route !== 'pages/login/index') {
-            //     // 未登陆
-            //     wx.navigateTo({
-            //       url: '../login/index'
-            //     })
-            //   }
-            // }
-            resolve(res)
-          },
-          fail(e: wx.GeneralCallbackResult) {
-            reject(e)
-          }
-        },
-        _options
-      ) as wx.RequestOption
-      wx.request(requestOptions)
+        fail: function(err) {
+          reject(err)
+        }
+      })
     })
   }
-})
+}
 
 export default Request
