d4d723ae99217f7f0cc0a48453a9f0c1c20ab90c
shine <xiaoshaoqq@gmail.com>
2019-07-31 17:59:26 +0800

    更新

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,3 @@
 export const APIKEY = '0df993c66c0c636e29ecbb5344252a4a'
 
-export const BASEHONST = 'http://localhost:3000'
-// export const BASEHONST = 'https://douban.uieee.com'
+export const api_url = 'http://www.shineshao.com'
